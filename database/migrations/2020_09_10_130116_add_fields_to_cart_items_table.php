b8e25ddcfa30aebeef078c278fb65555c07a8a00
MISHI RAY <42464035+mishiray@users.noreply.github.com>
2020-09-10 15:32:47 +0100

    nullable

---
+++ database/migrations/2020_09_10_130116_add_fields_to_cart_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cart_items', function (Blueprint $table) {
+            $table->decimal('price')->after('quantity');
+            $table->decimal('discount')->after('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cart_items', function (Blueprint $table) {
+            //
+        });
+    }
+}
