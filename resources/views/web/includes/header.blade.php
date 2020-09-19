3c322e91bd9da17e68ecdd61d4af687697d5bc5e
MISHI RAY <42464035+mishiray@users.noreply.github.com>
2020-09-19 10:39:57 +0100

    added route search

--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -41,7 +41,7 @@
             <div class="header-right">
                 <div class="header-search">
                     <a href="#" class="search-toggle" role="button" title="Search"><i class="icon-search"></i></a>
-                    <form action="#" method="get">
+                    <form action="{{route('search')}}" method="get">
                         <div class="header-search-wrapper">
                             <label for="q" class="sr-only">Search</label>
                             <input type="search" class="form-control" name="q" id="q" placeholder="Search in..." required>
